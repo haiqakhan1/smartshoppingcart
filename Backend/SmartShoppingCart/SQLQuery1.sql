1d977a2c1b515fc3d7c610be8253a0f5561418e1
Ayesha Shahid <ayeshashahid3345@gmail.com>
2025-12-14 15:07:41 +0500

    Backend code for Smart Shopping Cart

---
+++ Backend/SmartShoppingCart/SQLQuery1.sql
@@ -0,0 +1,7 @@
+﻿INSERT INTO dbo.Products (Name, Price, Quantity, Barcode)
+VALUES
+('Cotton Pads', 120.00, 10, '8964001596400'),
+('Rose Water', 200.00, 10, '8964000057308'),
+('Lays Masala', 70.00, 10, '8964002346912'),
+('Cocomo', 50.00, 10, '8961103502278'),
+('Moisturizer', 1000.00, 10, '8964004135033');
---
+++ Backend/SmartShoppingCart/SmartShoppingCart/Controllers/ProductController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace SmartShoppingCart.Controllers
+{
+    // This controller handles API requests for /api/products
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        // Connection string for the SQL database
+        private readonly string connectionString;
+
+        public ProductsController(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        // This endpoint returns the product with the matching barcode.
+        [HttpGet("barcode/{barcode}")]
+        public IActionResult GetByBarcode(string barcode)
+        {
+            using (var conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "SELECT Id, Name, Price, Quantity,Barcode FROM Products WHERE Barcode = @barcode";
+                using (var cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@barcode", barcode);
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            // Return product data as JSON
+                            return Ok(new
+                            {
+                                id = reader["Id"],
+                                name = reader["Name"].ToString(),
+                                price = Convert.ToDecimal(reader["Price"]),
+                                quantity = Convert.ToInt32(reader["Quantity"]),
+                                barcode = reader["Barcode"].ToString()
+
+                            });
+                        }
+                    }
+                }
+            }
+            return NotFound();
+        }
+    }
+}
